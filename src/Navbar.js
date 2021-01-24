fe22acf781b828247d6b5dd72dd9a1d4359b0343
Your Name <delftkarmilan@gmail.com>
2021-01-25 00:09:52 +0530

    version-1

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>kBlogs</h1>
+            <div className="content">
+            <a href="http://">Home</a>&nbsp;
+            <a href="http://">add-new</a>
+        </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
