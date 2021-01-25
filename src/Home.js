909e49bc02283190ac15dc7ca6f039a85a73c28b
Your Name <delftkarmilan@gmail.com>
2021-01-25 14:19:19 +0530

    click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+
+    const click = (e) =>{
+        console.log('hi ',e);
+}
+
+    const hclick = (name,e) =>{
+            console.log('hello ' +name, e.target);
+    }
+
     return (
         <div className="home">
-            <h1>home</h1>
+            <button onClick={click}>click</button>
+
+            <button onClick={(e) => {hclick('kar',e)}}>click here</button>
+            
         </div>
     );
 }
